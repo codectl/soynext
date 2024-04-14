9e540660332738ce0073a7c2877867ec77f588f7
Renato Damas <me@renatodamas.com>
2024-04-14 10:05:09 -0500

    chore: remove JStype

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import("tailwindcss").Config} */
 export default {
   darkMode: "class",
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui")
+  ],
 }
